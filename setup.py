63acca15850003739b832ef22e1df8a2d6e0f369
m33mt33n <m3@gmx.com>
2025-04-09 02:10:55 +0500

    setup.py: update

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 
 from setuptools import setup
 
+with open('requirements.txt') as f:
+  install_requires = [d.strip() for d in f.read().strip().splitlines()]
+
 setup(
   name='nguess',
   version='1.0',
-  description='A number guess game',
+  description='Simulation of a schooltime number guess game',
   long_description=open('README.md').read(),
   author='m33mt33n',
   author_email='m.3@gmx.com',
@@ -12,18 +15,19 @@ setup(
   license='GPLv3+',
   packages=['nguess'],
   package_dir={'nguess': 'src'},
-  install_requires=['colored', 'tabulate'],
+  python_requires=">=3.7",
+  install_requires=install_requires,
   entry_points={
     'console_scripts': ['nguess = nguess.cli:main'],
   },
   classifiers=[  # see https://pypi.python.org/pypi?%3Aaction=list_classifiers
     'Topic :: Games/Entertainment :: Simulation',
     'Environment :: Console',
-    'License :: OSI Approved :: GPLv3+ License',
+    'License :: GPLv3+ License',
     'Operating System :: OS Independent',
     'Natural Language :: English',
-    'Programming Language :: Python :: 3',
     'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
     'Development Status :: 5 - Production/Stable',
   ],
 )
